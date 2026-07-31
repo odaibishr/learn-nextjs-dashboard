f8e241c0672d1a9673717de4b3f987116d0217b9
Odai Bishr <odaibishr@gmail.com>
2026-07-31 21:01:40 +0300

    chore: add ESLint configuration with Next.js core web vitals support

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { CustomerField } from "@/app/lib/definitions";
 import Link from "next/link";
 import {
@@ -7,11 +9,15 @@ import {
 	UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import { Button } from "@/app/ui/button";
-import { createInvoice } from "@/app/lib/actions";
+import { createInvoice, State } from "@/app/lib/actions";
+
+import { useActionState } from "react";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+	const initialState: State = { message: null, errors: {} };
+	const [state, formAction] = useActionState(createInvoice, initialState);
 	return (
-		<form action={createInvoice}>
+		<form action={formAction}>
 			<div className="rounded-md bg-gray-50 p-4 md:p-6">
 				{/* Customer Name */}
 				<div className="mb-4">
@@ -36,6 +42,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 						</select>
 						<UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
 					</div>
+					<div id="customer-error" aria-live="polite" aria-atomic="true">
+						{state.errors?.customerId &&
+							state.errors.customerId.map((error: string) => (
+								<p className="mt-2 text-sm text-red-500" key={error}>
+									{error}
+								</p>
+							))}
+					</div>
 				</div>
 
 				{/* Invoice Amount */}
@@ -55,6 +69,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 							/>
 							<CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
 						</div>
+					</div>
+					<div id="amount-error" aria-live="polite" aria-atomic>
+						{state.errors?.amount &&
+							state.errors.amount.map((error: string) => (
+								<p className="mt-2 text-sm text-red-500" key={error}>
+									{error}
+								</p>
+							))}
 					</div>
 				</div>
 
@@ -97,8 +119,12 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 							</div>
 						</div>
 					</div>
+					<div id="status-error" className="mt-2 text-sm text-red-500">
+						{state.errors?.status}
+					</div>
 				</fieldset>
 			</div>
+
 			<div className="mt-6 flex justify-end gap-4">
 				<Link
 					href="/dashboard/invoices"
